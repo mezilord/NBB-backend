96771a76fd824acaf8c0d53d9845a63865efff2f
Muhammad Zafar <mzafar611@gmail.com>
2023-10-03 11:10:34 +0500

    commit

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import {AnswerRouter} from "./routes/answer.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(AnswerRouter);
 
-const uri = "mongodb+srv://nbb:nbb@cluster0.8nsytmz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.PASSWORD}@cluster0.8nsytmz.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(uri);
 
 mongoose.connection.on("connected", () => {
@@ -18,4 +20,6 @@ mongoose.connection.on("connected", () => {
 
 app.listen(3001, () => {
   console.log(`Server is running on port 3001`);
+  console.log('username', process.env.DB_USERNAME)
+  console.log('password', process.env.PASSWORD)
 });
